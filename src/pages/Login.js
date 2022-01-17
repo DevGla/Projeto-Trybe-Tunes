668ab1b08d5b397f574c647758cb1cef0bbed96e
george lucas <glalmeida.dev@gmail.com>
2022-01-17 16:38:09 -0300

    Requisito 2

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,8 +1,78 @@
-import React from 'react';
+import React from "react";
+import { Redirect } from "react-router-dom";
+import { createUser } from "../services/userAPI";
+import Carregando from "./Carregando";
+
+const MAX_TAM_VALUE = 2;
 
 class Login extends React.Component {
+  constructor() {
+    super();
+
+    this.state = {
+      nomeDigitado: "",
+      buttonDisabled: true,
+      loading: false,
+      login: false,
+    };
+
+    this.handleChange = this.handleChange.bind(this);
+    this.handleClick = this.handleClick.bind(this);
+  }
+
+  handleChange(evento) {
+    const { nomeDigitado } = this.state;
+    this.setState({ nomeDigitado: evento.target.value });
+    if (nomeDigitado.length >= MAX_TAM_VALUE) {
+      this.setState({ buttonDisabled: false });
+    }
+  }
+
+  async handleClick() {
+    const { nomeDigitado } = this.state;
+    this.setState({ loading: true });
+    await createUser({ name: nomeDigitado });
+    this.setState({
+      loading: false,
+      login: true,
+    });
+  }
+
   render() {
-    return <div data-testid="page-login"> </div>;
+    const { nomeDigitado, buttonDisabled, loading, login } = this.state;
+    return (
+      <div data-testid="page-login">
+        {loading ? (
+          <Carregando />
+        ) : (
+          <form>
+            <label htmlFor="nome">
+              Digite seu nome:
+              <input
+                type="text"
+                value={ nomeDigitado }
+                id="nome"
+                data-testid="login-name-input"
+                name="name"
+                onChange={ this.handleChange }
+              />
+            </label>
+            <label htmlFor="button">
+              <input
+                type="button"
+                data-testid="login-submit-button"
+                value=""
+                id="button"
+                disabled={buttonDisabled}
+                onClick={this.handleClick}
+              />
+              Entrar
+            </label>
+            {login ? <Redirect to="/search" /> : ''}
+          </form>
+        )}
+      </div>
+    );
   }
 }
 
--- src/pages/Search.js
+++ src/pages/Search.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 class Search extends React.Component {
   render() {
-    return <div data-testid="page-search"> </div>;
+    return (<div data-testid="page-search">Vamos com tudo! </div>);
   }
 }
 
