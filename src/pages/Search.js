6f06509f9f9df154b5dea0c37fc72d081b881676
george lucas <glalmeida.dev@gmail.com>
2022-01-18 10:30:17 -0300

    requisito 5

--- src/pages/Search.js
+++ src/pages/Search.js
@@ -2,11 +2,54 @@ import React from 'react';
 import Header from '../components/Header';
 
 class Search extends React.Component {
+  constructor() {
+    super();
+
+    this.state = {
+      artistaDigitado: '',
+      buttonDisabled: true,
+    };
+    this.handleChange = this.handleChange.bind(this);
+  }
+
+  handleChange(evento) {
+    const { artistaDigitado } = this.state;
+    this.setState({ artistaDigitado: evento.target.value });
+    if (artistaDigitado.length >= 1) {
+      this.setState({ buttonDisabled: false });
+    }
+  }
+
   render() {
+    const { buttonDisabled, artistaDigitado } = this.state;
     return (
       <div data-testid="page-search">
         <Header />
-      </div>);
+        <form>
+          <label htmlFor="artista">
+            <input
+              type="text"
+              id="artista"
+              data-testid="search-artist-input"
+              name="artista"
+              onChange={ this.handleChange }
+              value={ artistaDigitado }
+            />
+          </label>
+          <label htmlFor="button">
+            <input
+              type="button"
+              data-testid="search-artist-button"
+              value=""
+              id="button"
+              disabled={ buttonDisabled }
+              // onClick={ this.handleClick }
+            />
+            Entrar
+          </label>
+        </form>
+      </div>
+    );
   }
 }
 
