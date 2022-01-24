a617d2af4f64adedb0b8b1404171ab631947e767
george lucas <glalmeida.dev@gmail.com>
2022-01-24 09:37:37 -0300

    Requisito 13

--- src/components/MusicCard.js
+++ src/components/MusicCard.js
@@ -1,28 +1,69 @@
 import React from 'react';
 import { PropTypes } from 'prop-types';
+import { addSong } from '../services/favoriteSongsAPI';
+import Carregando from '../pages/Carregando';
 
 class MusicCard extends React.Component {
+  constructor() {
+    super();
+
+    this.state = {
+      loading: false,
+      isFavorite: false,
+    };
+
+    this.handleClick = this.handleClick.bind(this);
+  }
+
+  handleClick = async ({ target: { checked } }) => {
+    const { infoAlbum } = this.props;
+    this.setState({ loading: true, isFavorite: checked });
+    const requisicao = await addSong(infoAlbum);
+    if (requisicao) this.setState({ loading: false });
+  }
+
   render() {
-    const {
-      infoAlbum,
-    } = this.props;
+    const { infoAlbum } = this.props;
+    const { loading, isFavorite } = this.state;
     return (
       <div>
-        {infoAlbum.slice(1).map((track) => (
-          <div key={ track.trackId }>
-            <p>{track.trackName}</p>
-            <audio data-testid="audio-component" src={ track.previewUrl } controls>
-              <track kind="captions" />
-              O seu navegador não suporta o elemento
-              <code>{track.previewUrl}</code>
-            </audio>
-          </div>
-        ))}
+        {loading ? (
+          <Carregando />
+        ) : (
+          infoAlbum.slice(1).map((track) => (
+            <div key={ track.trackId }>
+              <p>{track.trackName}</p>
+              <audio
+                data-testid="audio-component"
+                src={ track.previewUrl }
+                controls
+              >
+                <track kind="captions" />
+                O seu navegador não suporta o elemento
+                <code>{track.previewUrl}</code>
+              </audio>
+              <label
+                htmlFor="favorita"
+              >
+                <input
+                  type="checkbox"
+                  data-testid={ `checkbox-music-${track.trackId}` }
+                  name="loading"
+                  id="favorita"
+                  checked={ isFavorite }
+                  onChange={ this.handleClick }
+                />
+                Favoritar
+              </label>
+            </div>
+          ))
+        )}
       </div>
     );
   }
 }
 
+// Verificar essa prop de um objeto que funcionou como string
 MusicCard.propTypes = {
   infoAlbum: PropTypes.string,
 };
--- src/pages/Profile.js
+++ src/pages/Profile.js
@@ -1,11 +1,62 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import Header from '../components/Header';
+import { getUser } from '../services/userAPI';
+import Carregando from './Carregando';
 
 class Profile extends React.Component {
+  constructor() {
+    super();
+
+    this.state = {
+      infoUser: {},
+      loading: false,
+    };
+  }
+
+  componentDidMount() {
+    const user = async () => {
+      this.setState({ loading: true });
+      const infoProfile = await getUser();
+      this.setState({ loading: false, infoUser: infoProfile });
+    };
+    user();
+  }
+
   render() {
+    const { loading, infoUser } = this.state;
+    const { name, email, image, description } = infoUser;
     return (
       <div data-testid="page-profile">
-        <Header />
+        <Header dataTestId="header-component" />
+        {loading ? (
+          <Carregando />
+        ) : (
+          <section>
+            <div>
+              <p>
+                Nome:
+                <span data-testid="header-user-name">{name}</span>
+              </p>
+              <p>
+                Email:
+                <span>{email}</span>
+              </p>
+              <p>
+                Descrição:
+                <span>{description}</span>
+              </p>
+              <img
+                src={ image }
+                alt={ name }
+                data-testid="profile-image"
+              />
+              <Link to="/profile/edit">
+                <button type="button">Editar perfil</button>
+              </Link>
+            </div>
+          </section>
+        )}
       </div>
     );
   }
